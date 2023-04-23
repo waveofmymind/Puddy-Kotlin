f36f9527f60c3cdf227093b7594875cb224a13d3
waveofmymind <sjun0913@gmail.com>
2023-04-24 00:06:04 +0900

    #1 [FEAT] 질문글 등록 테스트 추가

---
+++ src/test/kotlin/com/team/puddy/question/service/QuestionServiceTest.kt
@@ -0,0 +1,55 @@
+package com.team.puddy.question.service
+
+import com.team.puddy.global.error.ErrorCode
+import com.team.puddy.global.error.exception.NotFoundException
+import com.team.puddy.question.domain.QuestionRepository
+import com.team.puddy.question.domain.questionRegister
+import com.team.puddy.question.dto.request.QuestionRegister
+import com.team.puddy.user.domain.User
+import com.team.puddy.user.domain.user
+import com.team.puddy.user.infrastructure.UserRepository
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.springframework.data.repository.findByIdOrNull
+
+
+class QuestionServiceTest : BehaviorSpec({
+
+    val questionRepository = mockk<QuestionRepository>(relaxed = true)
+    val userRepository = mockk<UserRepository>(relaxed = true)
+    val questionService = QuestionService(questionRepository, userRepository)
+
+    Given("질문글을 등록할 때") {
+        val userId = 1L
+        val questionRegister = questionRegister()
+        val user = user()
+
+        every { userRepository.findByIdOrNull(userId) } returns user
+        every { questionRepository.save(any()) } returnsArgument 0
+
+        When("모든 정보가 있으면") {
+            questionService.registerQuestion(questionRegister, userId)
+
+            Then("질문글이 등록된다.") {
+                verify { questionRepository.save(any()) }
+            }
+        }
+
+        When("유저가 없는 유저일 경우") {
+            every { userRepository.findByIdOrNull(userId) } throws NotFoundException(ErrorCode.USER_NOT_FOUND)
+
+            Then("Not Found 예외가 발생한다.") {
+                val exception = shouldThrow<NotFoundException> {
+                    questionService.registerQuestion(questionRegister, userId)
+                }
+                exception.errorCode shouldBe ErrorCode.USER_NOT_FOUND
+            }
+        }
+    }
+
+
+})
