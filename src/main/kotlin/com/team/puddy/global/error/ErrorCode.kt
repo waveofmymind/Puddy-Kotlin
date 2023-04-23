c86104b662c59b2f76f7d09b228bd85a0fe3878a
waveofmymind <sjun0913@gmail.com>
2023-04-24 00:03:30 +0900

    #4 [FEAT] 유저 서비스 예외 공통 처리 추가

--- src/main/kotlin/com/team/puddy/global/error/ErrorCode.kt
+++ src/main/kotlin/com/team/puddy/global/error/ErrorCode.kt
@@ -5,7 +5,17 @@ import org.springframework.http.HttpStatus
 
 enum class ErrorCode(val httpStatus: HttpStatus, val message: String) {
 
-    DUPLICATE_REGISTER(HttpStatus.CONFLICT, "이미 가입된 유저입니다.")
+    //USER
+    DUPLICATE_REGISTER(HttpStatus.CONFLICT, "이미 가입된 유저입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
+    INVALID_PASSWORD(HttpStatus.CONFLICT, "비밀번호가 일치하지 않습니다."),
+
+    //QUESTION
+    QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지 않은 문제입니다."),
+
+    //TOKEN
+    TOKEN_VERIFY_FAIL(HttpStatus.UNAUTHORIZED, "토큰 검증에 실패하였습니다."),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "리프레시 토큰을 찾을 수 없습니다."),
 
 
 }
--- src/main/kotlin/com/team/puddy/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/team/puddy/global/error/GlobalExceptionHandler.kt
@@ -1,7 +1,9 @@
 package com.team.puddy.global.error
 
-import com.team.puddy.global.error.exception.Response
+import com.team.puddy.global.common.api.Response
+import com.team.puddy.global.error.exception.NotFoundException
 import com.team.puddy.global.error.exception.user.DuplicateRegisterException
+import com.team.puddy.global.error.exception.user.InvalidPasswordException
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -11,6 +13,16 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateRegisterException::class)
     fun duplicateRegisterException(e: DuplicateRegisterException): ResponseEntity<Response<Nothing>> {
+        return ResponseEntity.status(e.errorCode.httpStatus).body(Response.fail(e.errorCode))
+    }
+
+    @ExceptionHandler(NotFoundException::class)
+    fun notFoundException(e: NotFoundException): ResponseEntity<Response<Nothing>> {
+        return ResponseEntity.status(e.errorCode.httpStatus).body(Response.fail(e.errorCode))
+    }
+
+    @ExceptionHandler(InvalidPasswordException::class)
+    fun invalidPasswordException(e: InvalidPasswordException): ResponseEntity<Response<Nothing>> {
         return ResponseEntity.status(e.errorCode.httpStatus).body(Response.fail(e.errorCode))
     }
 }
