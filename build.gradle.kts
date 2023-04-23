e51be2c2f1567b04b51af30db7725dbb0222d66d
waveofmymind <sjun0913@gmail.com>
2023-04-24 00:01:26 +0900

    [FEAT] 의존성 추가
    - kotest, mockk
    - JPA
    - S3
    - kotlin-jdk

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
 plugins {
     id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
+    id ("org.jetbrains.kotlin.plugin.allopen") version "1.6.21"
     kotlin("jvm") version "1.8.20"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
@@ -35,29 +37,50 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.mysql:mysql-connector-j")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.2.Final")
     testImplementation("org.springframework.security:spring-security-test")
 
+    testImplementation("io.mockk:mockk:1.13.5")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.mapstruct:mapstruct:1.5.2.Final")
     kapt("org.mapstruct:mapstruct-processor:1.5.2.Final")
     kaptTest("org.mapstruct:mapstruct-processor:1.5.2.Final")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
+    testImplementation("io.kotest:kotest-assertions-core:4.6.0")
+    testImplementation("io.mockk:mockk:1.11.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+
+    //S3
+    implementation("io.awspring.cloud:spring-cloud-starter-aws:2.4.4")
+
 
     //token
     implementation ("com.auth0:java-jwt:3.18.3")
     implementation ("io.jsonwebtoken:jjwt:0.9.1")
     implementation ("com.nimbusds:nimbus-jose-jwt:9.31")
+}
+//JPA 설정
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
 }
 
 tasks.withType<KotlinCompile> {
@@ -67,7 +90,7 @@ tasks.withType<KotlinCompile> {
     }
 }
 
-tasks.withType<Test> {
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
 
