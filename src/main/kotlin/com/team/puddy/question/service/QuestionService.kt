64515a0f7c0d954b1cf54cc227d8ac051c3d44c6
waveofmymind <sjun0913@gmail.com>
2023-04-24 00:02:28 +0900

    #1 [FEAT] 질문글 등록 메서드 추가

--- src/main/kotlin/com/team/puddy/question/service/QuestionService.kt
+++ src/main/kotlin/com/team/puddy/question/service/QuestionService.kt
@@ -1,19 +1,38 @@
 package com.team.puddy.question.service
 
+import com.team.puddy.global.error.ErrorCode
+import com.team.puddy.global.error.exception.NotFoundException
 import com.team.puddy.question.domain.QuestionRepository
-import com.team.puddy.question.dto.request.RegisterQuestion
-import lombok.RequiredArgsConstructor
+import com.team.puddy.question.dto.request.QuestionRegister
+import com.team.puddy.question.dto.request.toEntity
+import com.team.puddy.question.dto.response.QuestionResponse
+import com.team.puddy.question.dto.response.toDto
+import com.team.puddy.user.infrastructure.UserRepository
+import jakarta.transaction.Transactional
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
-@RequiredArgsConstructor
 class QuestionService(
-    private val questionRepository: QuestionRepository
-) {
+    private val questionRepository: QuestionRepository,
+    private val userRepository : UserRepository,
+  ) {
 
-    fun registerQuestion(registerQuestion: RegisterQuestion): Unit {
+    @Transactional
+    fun registerQuestion(questionRegister: QuestionRegister, userId: Long) {
+        val findUser = userRepository.findByIdOrNull(userId) ?: throw NotFoundException(ErrorCode.USER_NOT_FOUND)
+        val question = questionRegister.toEntity(findUser)
+        questionRepository.save(question)
+    }
 
+    @Transactional
+    fun getQuestion(id: Long) : QuestionResponse {
+        val question = questionRepository.findByIdOrNull(id) ?: throw NotFoundException(ErrorCode.QUESTION_NOT_FOUND)
+
+        return toDto(question)
 
 
     }
+
+
 }
