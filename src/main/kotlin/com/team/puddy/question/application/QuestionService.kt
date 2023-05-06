26d49a55ce2ddb63bf23f753075be63ebebb0223
waveofmymind <sjun0913@gmail.com>
2023-05-07 00:14:02 +0900

    [FIX] 서비스 로직 코틀린 스코프 함수 사용

--- src/main/kotlin/com/team/puddy/question/application/QuestionService.kt
+++ src/main/kotlin/com/team/puddy/question/application/QuestionService.kt
@@ -1,38 +1,39 @@
 package com.team.puddy.question.application
 
 import com.team.puddy.global.error.ErrorCode
-import com.team.puddy.global.error.exception.NotFoundException
+import com.team.puddy.global.error.QuestionNotFoundException
+import com.team.puddy.global.error.UserNotFoundException
 import com.team.puddy.question.domain.QuestionRepository
-import com.team.puddy.question.dto.request.QuestionRegister
-import com.team.puddy.question.dto.request.toEntity
-import com.team.puddy.question.dto.response.QuestionResponse
-import com.team.puddy.question.dto.response.toDto
+import com.team.puddy.question.dto.QuestionRegister
+import com.team.puddy.question.dto.QuestionResponse
+import com.team.puddy.question.dto.toDto
+import com.team.puddy.question.dto.toEntity
 import com.team.puddy.user.infrastructure.UserRepository
-import jakarta.transaction.Transactional
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class QuestionService(
     private val questionRepository: QuestionRepository,
-    private val userRepository : UserRepository,
-  ) {
+    private val userRepository: UserRepository,
+) {
 
     @Transactional
     fun registerQuestion(questionRegister: QuestionRegister, userId: Long) {
-        val findUser = userRepository.findByIdOrNull(userId) ?: throw NotFoundException(ErrorCode.USER_NOT_FOUND)
-        val question = questionRegister.toEntity(findUser)
-        questionRepository.save(question)
+        userRepository.findByIdOrNull(userId)?.also { findUser ->
+            questionRegister.toEntity(findUser).also { question ->
+                questionRepository.save(question)
+            }
+        } ?: throw UserNotFoundException(ErrorCode.USER_NOT_FOUND)
     }
 
-    @Transactional
-    fun getQuestion(id: Long) : QuestionResponse {
-        val question = questionRepository.findByIdOrNull(id) ?: throw NotFoundException(ErrorCode.QUESTION_NOT_FOUND)
-
-        return toDto(question)
-
-
+    @Transactional(readOnly = true)
+    fun getQuestion(id: Long): QuestionResponse {
+        return run {
+            val question = questionRepository.findByIdOrNull(id)
+                ?: throw QuestionNotFoundException(ErrorCode.QUESTION_NOT_FOUND)
+            toDto(question)
+        }
     }
-
-
 }
