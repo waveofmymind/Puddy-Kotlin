89c5ee494c785f2b26bc6d97ed6945af6764ccc3
waveofmymind <sjun0913@gmail.com>
2023-05-06 23:57:27 +0900

    [FIX] 비즈니스 예외 코틀린 파일로 변경

--- src/main/kotlin/com/team/puddy/global/error/exception/BusinessException.kt
+++
@@ -1,5 +0,0 @@
-package com.team.puddy.global.error.exception
-
-import com.team.puddy.global.error.ErrorCode
-
-open class BusinessException(val errorCode: ErrorCode, message: String? = null) : RuntimeException(message)
