d804186ac3ee48defc7d9855887673b57797d3bd
waveofmymind <sjun0913@gmail.com>
2023-05-07 00:13:46 +0900

    [FIX] DTO 코틀린 파일로 모집화

---
+++ src/main/kotlin/com/team/puddy/question/dto/QuestionRequest.kt
@@ -0,0 +1,27 @@
+package com.team.puddy.question.dto
+
+import com.team.puddy.question.domain.Category
+import com.team.puddy.question.domain.Question
+import com.team.puddy.user.domain.User
+
+data class QuestionRegister(
+    val title : String,
+    val content : String,
+    val postCategory : Int,
+    val category : String, )
+
+fun QuestionRegister.toEntity(user : User) = Question(
+    title = title,
+    content = content,
+    postCategory = postCategory,
+    isSolved = false,
+    user = user,
+    category = Category.valueOf(category),
+)
+
+data class QuestionModify(
+    val title : String,
+    val content : String,
+    val postCategory : Int,
+    val category : String, )
+//TODO: 이미지 추가
