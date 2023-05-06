b1612222edad54179eeeeac11030ff7a56f01a63
waveofmymind <sjun0913@gmail.com>
2023-05-07 00:14:24 +0900

    [FIX] 사용하지 않는 예외 삭제

--- src/main/kotlin/com/team/puddy/global/error/exception/NotFoundException.kt
+++
@@ -1,6 +0,0 @@
-package com.team.puddy.global.error.exception
-
-import com.team.puddy.global.error.ErrorCode
-
-class NotFoundException(errorCode: ErrorCode) : BusinessException(errorCode) {
-}
--- src/main/kotlin/com/team/puddy/global/error/exception/user/DuplicateRegisterException.kt
+++
@@ -1,9 +0,0 @@
-package com.team.puddy.global.error.exception.user
-
-import com.team.puddy.global.error.ErrorCode
-import com.team.puddy.global.error.exception.BusinessException
-
-class DuplicateRegisterException : BusinessException {
-    constructor(errorCode: ErrorCode) : super(errorCode,errorCode.message)
-    constructor(errorCode: ErrorCode, message: String) : super(errorCode, message)
-}
