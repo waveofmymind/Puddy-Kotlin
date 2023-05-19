18f18f2a30df9b73bd747a2f71559fe00b22d864
waveofmymind <sjun0913@gmail.com>
2023-05-19 20:24:14 +0900

    [FEAT] url 시크릿화

--- src/main/kotlin/com/team/puddy/global/slack/SlackService.kt
+++ src/main/kotlin/com/team/puddy/global/slack/SlackService.kt
@@ -2,18 +2,22 @@ package com.team.puddy.global.slack
 
 import net.gpedro.integrations.slack.SlackApi
 import net.gpedro.integrations.slack.SlackMessage
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
-class SlackService {
+class SlackService(
+    @Value("\${slack.webhook.url}")
+    private val url: String
+
+) {
 
     fun sendSlackMessage(username : String) {
         val message = """
             $username 님이 문서를 보냈습니다! 확인해주세요!
         """.trimIndent()
-        val url = "https://hooks.slack.com/services/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"
         SlackApi(url).call(SlackMessage(message))
     }
 }
