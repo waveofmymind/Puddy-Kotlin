41fc3a531d71621ddbc021eb731b6fd20682817e
waveofmymind <sjun0913@gmail.com>
2023-05-21 13:50:32 +0900

    [FEAT] 파라미터 없는 성공 메서드 추가

--- src/main/kotlin/com/team/puddy/global/common/api/Response.kt
+++ src/main/kotlin/com/team/puddy/global/common/api/Response.kt
@@ -4,7 +4,7 @@ import com.team.puddy.global.error.ErrorCode
 import org.springframework.http.HttpStatus
 
 data class Response<T>(
-    val code : Int,
+    val code: Int,
     val status: HttpStatus,
     val message: String,
     var data: T? = null
@@ -12,18 +12,29 @@ data class Response<T>(
 
     companion object {
 
-        @JvmStatic fun <T> of(httpStatus: HttpStatus, message: String, data: T): Response<T> {
+        @JvmStatic
+        fun <T> of(httpStatus: HttpStatus, message: String, data: T): Response<T> {
             return Response(httpStatus.value(), httpStatus, message, data)
         }
 
-       @JvmStatic fun <T> of(httpStatus: HttpStatus, data: T): Response<T> {
+        @JvmStatic
+        fun <T> of(httpStatus: HttpStatus, data: T): Response<T> {
             return of(httpStatus, httpStatus.name, data)
         }
 
-       @JvmStatic fun <T> ok(data: T): Response<T> {
-            return of(HttpStatus.OK, data)
+        @JvmStatic
+        fun <T> ok(data: T): Response<T> {
+            return of(HttpStatus.OK,HttpStatus.OK.name, data)
         }
-        @JvmStatic fun <T> fail(errorCode: ErrorCode): Response<T?> {
+
+        @JvmStatic
+        fun ok(): Response<Unit> {
+            return of(HttpStatus.OK, HttpStatus.OK.name, Unit)
+        }
+
+
+        @JvmStatic
+        fun <T> fail(errorCode: ErrorCode): Response<T?> {
             return of(errorCode.httpStatus, errorCode.message, null)
         }
 
